4eacf0da5ff199e7335be4d5fbe0b15826adc123
Athindra Mitra <athindra.nm4@gmail.com>
2025-11-26 16:56:40 -0500

    asks for combo and tells it back

--- src/main.cpp
+++ src/main.cpp
@@ -3,12 +3,21 @@
 
 int main() {
 	std::cout << "[--- BEYBLADE X ENGINE ---]\n";
+
 	std::cout << "\nEnter the parts you want to combine:\n";
+
 	std::cout << "Blade: ";
-	
 	std::string blade {};
 	std::getline(std::cin >> std::ws, blade);
 
-	std::cout << blade << std::endl;
-
+	std::cout << "Ratchet: ";
+	std::string ratchet {};
+	std::getline(std::cin >> std::ws, ratchet);
+	
+	std::cout << "Bit: ";
+	std::string bit {};
+	std::getline(std::cin >> std::ws, bit);
+	
+	std::string combo {blade + ' ' + ratchet + bit};
+	std::cout << "Your combo is:\n" << combo << '\n';
 }
