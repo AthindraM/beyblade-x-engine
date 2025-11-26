b0847437117e455fc1ed55237ba8fa7935d7284a
Athindra Mitra <athindra.nm4@gmail.com>
2025-11-26 17:46:06 -0500

    created menu and functions and allowed user to exit program

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,7 @@
 #include <iostream>
 #include <string>
 
-int main() {
-	std::cout << "[--- BEYBLADE X ENGINE ---]\n";
-
+void createCombo() {
 	std::cout << "\nEnter the parts you want to combine:\n";
 
 	std::cout << "Blade: ";
@@ -19,5 +17,32 @@ int main() {
 	std::getline(std::cin >> std::ws, bit);
 	
 	std::string combo {blade + ' ' + ratchet + bit};
-	std::cout << "Your combo is:\n" << combo << '\n';
+	std::cout << "\nYour combo is:\n" << combo << '\n';
+}
+
+void viewCombos() { std::cout << "\ncoming soon!\n"; }
+
+int main() {
+	bool running {true};
+	while (running) {
+		std::cout << "\n[--- BEYBLADE X ENGINE ---]\n";
+		std::cout << "1. Create Combo\n2. View Saved Combos\n3. Exit\n";
+	
+		int choice {};
+		std::cin >> choice;
+		switch (choice) {
+			case 1: {
+				createCombo();
+				break;
+			}
+			case 2: {
+				viewCombos();
+				break;
+			}
+			case 3: {
+				running = false;
+				break;
+			}
+		}
+	}
 }
