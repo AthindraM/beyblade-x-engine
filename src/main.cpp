5ac8f5e12309c4f1e852026425839678d4c8a6b6
Athindra Mitra <athindra.nm4@gmail.com>
2025-11-26 16:31:25 -0500

    added main

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+int main() {
+	std::cout << "[--- BEYBLADE X ENGINE ---]\n";
+	std::cout << "\nEnter the parts you want to combine:\n";
+	std::cout << "Blade: ";
+	
+	std::string blade {std::cin >> std:ws};
+
+	std::cout << blade << std::endl;
+
+}
