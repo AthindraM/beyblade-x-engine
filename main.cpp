ab62ae8e0fe578995eeb50bb0fc948a63eaa93a0
Athindra Mitra <athindra.nm4@gmail.com>
2026-08-13 15:09:31 -0400

    created main menu

--- main.cpp
+++ main.cpp
@@ -1,3 +1,27 @@
 #include <iostream>
 
-int main() { std::cout << "Beyblade X Engine\n"; }
+int main() {
+  std::cout << "[\n----Beyblade X Engine----]\n";
+  std::cout << "===========================\n";
+
+  while (true) {
+    std::cout << "1. Manage Combos\n";
+    std::cout << "2. Manage Decks\n";
+    std::cout << "3. Exit\n";
+
+    int choice{};
+    std::cin >> choice;
+
+    switch (choice) {
+    case 1:
+      break;
+    case 2:
+      break;
+    case 3:
+      return 0;
+    default:
+      std::cout << "Invalid option, try again.\n";
+      break;
+    }
+  }
+}
