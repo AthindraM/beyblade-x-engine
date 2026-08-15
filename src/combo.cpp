04984a0b1dbdf4d7ef24af5e80a47b900cad0255
Athindra Mitra <athindra.nm4@gmail.com>
2026-08-14 23:44:57 -0400

    combo file completed

---
+++ src/combo.cpp
@@ -0,0 +1,66 @@
+#include "Combo.h"
+
+#include <iostream>
+#include <sstream>
+#include <vector>
+
+Combo::Combo() : id(0), name(""), blade(""), ratchet(""), bit("") {}
+
+Combo::Combo(int id, const std::string &name, const std::string &blade,
+             const std::string &ratchet, const std::string &bit)
+    : id(id), name(name), blade(blade), ratchet(ratchet), bit(bit) {}
+
+int Combo::getId() const { return id; }
+
+const std::string &Combo::getName() const { return name; }
+
+const std::string &Combo::getBlade() const { return blade; }
+
+const std::string &Combo::getRatchet() const { return ratchet; }
+
+const std::string &Combo::getBit() const { return bit; }
+
+void Combo::setName(const std::string &name) { this->name = name; }
+
+void Combo::setBlade(const std::string &blade) { this->blade = blade; }
+
+void Combo::setRatchet(const std::string &ratchet) { this->ratchet = ratchet; }
+
+void Combo::setBit(const std::string &bit) { this->bit = bit; }
+
+void Combo::display() const {
+  std::cout << "\n";
+  std::cout << "ID:      " << id << '\n';
+  std::cout << "Name:    " << name << '\n';
+  std::cout << "Blade:   " << blade << '\n';
+  std::cout << "Ratchet: " << ratchet << '\n';
+  std::cout << "Bit:     " << bit << '\n';
+}
+
+std::string Combo::serialize() const {
+  std::ostringstream output;
+
+  output << id << '|' << name << '|' << blade << '|' << ratchet << '|' << bit;
+
+  return output.str();
+}
+
+Combo Combo::deserialize(const std::string &line) {
+  std::stringstream stream(line);
+
+  std::string idString;
+  std::string name;
+  std::string blade;
+  std::string ratchet;
+  std::string bit;
+
+  std::getline(stream, idString, '|');
+  std::getline(stream, name, '|');
+  std::getline(stream, blade, '|');
+  std::getline(stream, ratchet, '|');
+  std::getline(stream, bit, '|');
+
+  int id = std::stoi(idString);
+
+  return Combo(id, name, blade, ratchet, bit);
+}
