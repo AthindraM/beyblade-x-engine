f94614294cdce793cbaeb325851317ff77525258
Athindra Mitra <athindra.nm4@gmail.com>
2026-08-15 00:05:21 -0400

    created combo menu

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,41 @@
 #include <iostream>
 
+void comboMenu() {
+  while (true) {
+    std::cout << "\n";
+    std::cout << "============================\n";
+    std::cout << "       COMBO MANAGER\n";
+    std::cout << "============================\n";
+
+    std::cout << "1. Create combo\n";
+    std::cout << "2. View all combos\n";
+    std::cout << "3. View combo\n";
+    std::cout << "4. Edit combo\n";
+    std::cout << "5. Delete combo\n";
+    std::cout << "0. Back\n";
+
+    int choice{};
+    std::cin >> choice;
+
+    switch (choice) {
+    case 1:
+      break;
+    case 2:
+      break;
+    case 3:
+      break;
+    case 4:
+      break;
+    case 5:
+      break;
+    case 0:
+      return;
+    default:
+      std::cout << "Invalid choice.\n";
+    }
+  }
+}
+
 int main() {
   while (true) {
     std::cout << "[\n----Beyblade X Engine----]\n";
@@ -13,6 +49,7 @@ int main() {
 
     switch (choice) {
     case 1:
+      comboMenu();
       break;
     case 2:
       break;
